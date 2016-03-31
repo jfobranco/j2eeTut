63814f2b7a813e4a5d8441085d4010dbe57f108d
João <jfobranco@gmail.com>
2016-03-31 18:16:44 +0200

    = Initial version.

---
+++ tp1/src/com/jb/beans/Customer.java
@@ -0,0 +1,41 @@
+package com.jb.beans;
+
+public class Customer {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String phone;
+	private String mail;
+
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+}
---
+++ tp1/src/com/jb/beans/Order.java
@@ -0,0 +1,57 @@
+package com.jb.beans;
+
+import java.util.Date;
+
+public class Order {
+	private Customer customer;
+	private Date date;
+	private double amount;
+	private String paymentMethod;
+	private String paymentStatus;
+	private String deliveryMode;
+	private String deliveryStatus;
+
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+	public String getPaymentStatus() {
+		return paymentStatus;
+	}
+	public void setPaymentStatus(String paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+	public String getDeliveryMode() {
+		return deliveryMode;
+	}
+	public void setDeliveryMode(String deliveryMode) {
+		this.deliveryMode = deliveryMode;
+	}
+	public String getDeliveryStatus() {
+		return deliveryStatus;
+	}
+	public void setDeliveryStatus(String deliveryStatus) {
+		this.deliveryStatus = deliveryStatus;
+	}
+}
