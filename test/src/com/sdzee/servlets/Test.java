68a24a60f65377f5fed08707d1fc56c9e82ed347
jfobranco <jfobranco@172.16.0.9>
2016-04-05 08:24:42 +0200

    = Remove unneeded imports in Test class.
    + Add jstl jar to projects.
    + Add jstlTest class to test project.

--- test/src/com/sdzee/servlets/Test.java
+++ test/src/com/sdzee/servlets/Test.java
@@ -1,9 +1,5 @@
 package com.sdzee.servlets;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
