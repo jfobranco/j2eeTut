83d608bb9f96f09f9b9ee366c6c073f42214b05b
jfobranco <jfobranco@Mac>
2016-04-18 08:36:02 +0200

    Start implementing cookie

--- testForms/src/com/sdzee/servlets/Connexion.java
+++ testForms/src/com/sdzee/servlets/Connexion.java
@@ -3,10 +3,18 @@ package com.sdzee.servlets;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.PeriodFormatter;
+import org.joda.time.format.PeriodFormatterBuilder;
 
 import com.sdzee.beans.Utilisateur;
 import com.sdzee.forms.ConnexionForm;
@@ -14,10 +22,33 @@ import com.sdzee.forms.ConnexionForm;
 public class Connexion extends HttpServlet {
 	public static final String ATT_USER = "utilisateur";
 	public static final String ATT_FORM = "form";
+	public static final String ATT_INTERVALLE_CONNEXIONS = "intervalleConnexions";
 	public static final String ATT_SESSION_USER = "sessionUtilisateur";
+	public static final String COOKIE_DERNIERE_CONNEXION = "derniereConnexion";
+	public static final String FORMAT_DATE = "dd/MM/yyyy HH:mm:ss";
 	public static final String VUE = "/WEB-INF/connexion.jsp";
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/* Tentative de récupération du cookie depuis la requête */
+		String derniereConnexion = getCookieValue(request, COOKIE_DERNIERE_CONNEXION);
+		/* Si le cookie existe, alors calcul de la durée */
+		if (derniereConnexion != null) {
+			/* Récupération de la date courante */
+			DateTime dtCourante = new DateTime();
+			/* Récupération de la date présente dans le cookie */
+			DateTimeFormatter formatter = DateTimeFormat.forPattern(FORMAT_DATE);
+			DateTime dtDerniereConnexion = formatter.parseDateTime(derniereConnexion);
+			/* Calcul de la durée de l'intervalle */
+			Period periode = new Period(dtDerniereConnexion, dtCourante);
+			/* Formatage de la durée de l'intervalle */
+			PeriodFormatter periodFormatter = new PeriodFormatterBuilder().appendYears().appendSuffix(" an ", " ans ")
+					.appendMonths().appendSuffix(" mois ").appendDays().appendSuffix(" jour ", " jours ").appendHours()
+					.appendSuffix(" heure ", " heures ").appendMinutes().appendSuffix(" minute ", " minutes ")
+					.appendSeparator("et ").appendSeconds().appendSuffix(" seconde", " secondes").toFormatter();
+			String intervalleConnexions = periodFormatter.print(periode);
+			/* Ajout de l'intervalle en tant qu'attribut de la requête */
+			request.setAttribute(ATT_INTERVALLE_CONNEXIONS, intervalleConnexions);
+		}
 		/* Affichage de la page de connexion */
 		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
 	}
@@ -47,5 +78,21 @@ public class Connexion extends HttpServlet {
 		request.setAttribute(ATT_USER, utilisateur);
 
 		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+	}
+
+	/**
+	 * Méthode utilitaire gérant la récupération de la valeur d'un cookie donné
+	 * depuis la requête HTTP.
+	 */
+	private static String getCookieValue(HttpServletRequest request, String nom) {
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie != null && nom.equals(cookie.getName())) {
+					return cookie.getValue();
+				}
+			}
+		}
+		return null;
 	}
 }
