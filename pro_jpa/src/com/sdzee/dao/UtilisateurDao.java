0399506f9b8aa6bb5cc677e9b32a2c45762b5b49
João <jfobranco@gmail.com>
2016-04-27 18:25:39 +0200

    Add pro_jpa project

---
+++ pro_jpa/src/com/sdzee/dao/UtilisateurDao.java
@@ -0,0 +1,43 @@
+package com.sdzee.dao;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import com.sdzee.entities.Utilisateur;
+
+@Stateless
+public class UtilisateurDao {
+	private static final String JPQL_SELECT_PAR_EMAIL = "SELECT u FROM Utilisateur u WHERE u.email=:email";
+	private static final String PARAM_EMAIL = "email";
+
+	// Injection du manager, qui s'occupe de la connexion avec la BDD
+	@PersistenceContext(unitName = "bdd_sdzee_PU")
+	private EntityManager em;
+
+	// Enregistrement d'un nouvel utilisateur
+	public void creer(Utilisateur utilisateur) throws DAOException {
+		try {
+			em.persist(utilisateur);
+		} catch (Exception e) {
+			throw new DAOException(e);
+		}
+	}
+
+	// Recherche d'un utilisateur à partir de son adresse email
+	public Utilisateur trouver(String email) throws DAOException {
+		Utilisateur utilisateur = null;
+		Query requete = em.createQuery(JPQL_SELECT_PAR_EMAIL);
+		requete.setParameter(PARAM_EMAIL, email);
+		try {
+			utilisateur = (Utilisateur) requete.getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		} catch (Exception e) {
+			throw new DAOException(e);
+		}
+		return utilisateur;
+	}
+}
---
+++ pro_jpa/src/com/sdzee/servlets/Inscription.java
@@ -0,0 +1,44 @@
+package com.sdzee.servlets;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sdzee.dao.UtilisateurDao;
+import com.sdzee.entities.Utilisateur;
+import com.sdzee.forms.InscriptionForm;
+
+@WebServlet(urlPatterns = { "/inscription" })
+public class Inscription extends HttpServlet {
+	public static final String ATT_USER = "utilisateur";
+	public static final String ATT_FORM = "form";
+	public static final String VUE = "/WEB-INF/inscription.jsp";
+
+	// Injection de notre EJB (Session Bean Stateless)
+	@EJB
+	private UtilisateurDao utilisateurDao;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/* Affichage de la page d'inscription */
+		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/* Préparation de l'objet formulaire */
+		InscriptionForm form = new InscriptionForm(utilisateurDao);
+
+		/* Traitement de la requête et récupération du bean en résultant */
+		Utilisateur utilisateur = form.inscrireUtilisateur(request);
+
+		/* Stockage du formulaire et du bean dans l'objet request */
+		request.setAttribute(ATT_FORM, form);
+		request.setAttribute(ATT_USER, utilisateur);
+
+		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+	}
+}
